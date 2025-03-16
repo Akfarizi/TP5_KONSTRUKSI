6916b9cc7a237709d7685cd875e58f3042b1f8ec
Akfarizi <muhammadalfarizi0204@gmail.com>
2025-03-16 22:24:42 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul5_103022300078;
+class Program
+{
+    static void Main(String[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Muhammad Akbar Alfarizi");
+    }
+}
